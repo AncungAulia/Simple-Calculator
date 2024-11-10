de0198b59adb7ee6bd77278ff9f04d2bac780c01
AncungAulia <ancungaulia@gmail.com>
2024-11-10 08:32:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+function satu(){
+    var data = document.getElementById('b1').value;
+    var dayaLayar = document.getElementById("layar").innerHTML;
+    document.getElementById("layar").innerHTML = dayaLayar + data;
+}
+
+function dua(){
+    var data = document.getElementById('b2').value;
+    var dayaLayar = document.getElementById("layar").innerHTML;
+    document.getElementById("layar").innerHTML = dayaLayar + data;
+}
+
+function tiga(){
+    var data = document.getElementById('b3').value;
+    var dayaLayar = document.getElementById("layar").innerHTML;
+    document.getElementById("layar").innerHTML = dayaLayar + data;
+}
+
+function empat(){
+    var data = document.getElementById('b4').value;
+    var dayaLayar = document.getElementById("layar").innerHTML;
+    document.getElementById("layar").innerHTML = dayaLayar + data;
+}
+
+function lima(){
+    var data = document.getElementById('b5').value;
+    var dayaLayar = document.getElementById("layar").innerHTML;
+    document.getElementById("layar").innerHTML = dayaLayar + data;
+}
+
+function enam(){
+    var data = document.getElementById('b6').value;
+    var dayaLayar = document.getElementById("layar").innerHTML;
+    document.getElementById("layar").innerHTML = dayaLayar + data;
+}
+
+function tujuh(){
+    var data = document.getElementById('b7').value;
+    var dayaLayar = document.getElementById("layar").innerHTML;
+    document.getElementById("layar").innerHTML = dayaLayar + data;
+}
+
+function delapan(){
+    var data = document.getElementById('b8').value;
+    var dayaLayar = document.getElementById("layar").innerHTML;
+    document.getElementById("layar").innerHTML = dayaLayar + data;
+}
+
+function sembilan(){
+    var data = document.getElementById('b9').value;
+    var dayaLayar = document.getElementById("layar").innerHTML;
+    document.getElementById("layar").innerHTML = dayaLayar + data;
+}
+
+function nol(){
+    var data = document.getElementById('b0').value;
+    var dayaLayar = document.getElementById("layar").innerHTML;
+    document.getElementById("layar").innerHTML = dayaLayar + data;
+}
+
+function tambah(){
+    var data = document.getElementById('b+').value;
+    var dayaLayar = document.getElementById("layar").innerHTML;
+    document.getElementById("layar").innerHTML = dayaLayar + data;
+}
+
+function kurang(){
+    var data = document.getElementById('b-').value;
+    var dayaLayar = document.getElementById("layar").innerHTML;
+    document.getElementById("layar").innerHTML = dayaLayar + data;
+}
+
+function kali(){
+    var data = document.getElementById('bx').value;
+    var dayaLayar = document.getElementById("layar").innerHTML;
+    document.getElementById("layar").innerHTML = dayaLayar + data;
+}
+
+function bagi(){
+    var data = document.getElementById('b/').value;
+    var dayaLayar = document.getElementById("layar").innerHTML;
+    document.getElementById("layar").innerHTML = dayaLayar + data;
+}
+
+function c(){
+    document.getElementById("layar").innerHTML = " ";
+}
+
+function sama(){
+var data = document.getElementById("layar").innerHTML;
+document.getElementById("layar").innerHTML = eval(data);
+}
